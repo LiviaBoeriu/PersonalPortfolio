abbaad4bf667bb05ffe9485e4109a67c17cf5db7
Livia Boeriu <boeriulivia@gmail.com>
2020-08-12 17:54:15 +0200

    Created animation for portfolio section on landing

---
+++ resources/home.js
@@ -0,0 +1,44 @@
+
+// Portfolio diagram
+const iconList = document.getElementsByClassName('icon');
+const iconLabels = ['Planning', 'Research', 'Requirements', 'Design', 'Implementation', 'Validation'];
+const textElement = document.getElementById('iconText');
+
+function showIcon() {
+    for(let i=0; i<iconList.length; i++) {
+        setTimeout(function () {
+            iconList[i].classList.remove('no-opacity');
+            textElement.innerHTML = iconLabels[i];
+            textElement.classList.remove('no-opacity');
+            
+            if(i === iconList.length - 1) {
+                setTimeout(hideIcon, 2500);
+            }
+        }, i*1000);
+    }
+}
+
+function hideIcon() {
+    for(let i=0; i<iconList.length; i++) {
+        setTimeout(function () {
+            iconList[i].classList.add('no-opacity');
+            textElement.innerHTML = iconLabels[i];
+            textElement.classList.add('no-opacity');
+            
+            if(i === iconList.length - 1) {
+                setTimeout(showIcon, 500);
+            }
+        }, i*150);
+    }
+}
+
+function checkPortfolioPosition() {
+    let portfolioSection = document.getElementsByClassName('portfolio-container')[0];
+    let portfolioSectionPosition = portfolioSection.getBoundingClientRect();
+    if(portfolioSectionPosition.y < 100 ) {
+        showIcon();
+        window.removeEventListener('scroll', checkPortfolioPosition);
+    }
+}
+
+window.addEventListener('scroll', checkPortfolioPosition);
--- resources/index.js
+++ resources/index.js
@@ -64,4 +64,3 @@ function changeSlide(isNext) {
 
     showSlides();
 }
-
