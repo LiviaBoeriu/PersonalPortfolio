fb55499710f84c72f57790971156506e75c86859
Livia Boeriu <boeriulivia@gmail.com>
2020-08-20 15:20:17 +0200

    Created hover and enlarge functionality for pics

--- resources/home.js
+++ resources/home.js
@@ -3,19 +3,72 @@
 const iconList = document.getElementsByClassName('icon');
 const iconLabels = ['Planning', 'Research', 'Requirements', 'Design', 'Implementation', 'Validation'];
 const textElement = document.getElementById('iconText');
+const scrollButton = document.getElementById('scroll-down-button');
+const downSection = document.getElementById('skills-container');
+const svg = document.getElementById('circleSvg');
+const svgMask = document.getElementById('mask');
+const navHeight = document.querySelector('header nav').offsetHeight;
+const rotateValues = [64, 122, 180, 241, 300, 366];
+
+
+// showIcons
+// - display text and icon (takes 250ms)
+// - animate line in (takes 600ms)
+// - display next text and icon (another 250ms)
+// - hide line and move it (250ms)
+
+
+// showIcons
+// - display text and icon each 1s 
+
+
+// showLine (delay 250ms)
+// - hide mask (AND STAY 1s)
+// - fadeOut (takes 250ms)
+// - show mask
+// - rotate
 
 function showIcon() {
     for(let i=0; i<iconList.length; i++) {
         setTimeout(function () {
+            // takes 250ms
             iconList[i].classList.remove('no-opacity');
-            textElement.innerHTML = iconLabels[i];
             textElement.classList.remove('no-opacity');
-            
+            textElement.innerHTML = iconLabels[i];
+
             if(i === iconList.length - 1) {
-                setTimeout(hideIcon, 2500);
+                setTimeout(hideIcon, 3000);
             }
-        }, i*1000);
+        }, i * 1250);
     }
+}
+
+function animateLine() {
+    let index = 1;
+
+    function animationProcess() {
+        svgMask.classList.add('hide')
+
+        setTimeout(() => {
+            svg.classList.add('fadeOut');
+
+            setTimeout(() => {
+                svgMask.classList.remove('hide');
+                svg.style.transform = `rotateZ(${rotateValues[index]}deg)`;
+                svg.classList.remove('fadeOut');
+
+                if (index !== rotateValues.length - 1) {
+                    index += 1;
+                } else {
+                    index = 0;
+                }
+            }, 250)
+        }, 1000)
+    }
+
+    animationProcess();
+    const animationLoop = setInterval(animationProcess, 1500)
+    // need to clear the interval (while hideIcons) and reset
 }
 
 function hideIcon() {
@@ -32,21 +85,18 @@ function hideIcon() {
     }
 }
 
+
 function checkPortfolioPosition() {
     let portfolioSection = document.getElementsByClassName('portfolio-container')[0];
     let portfolioSectionPosition = portfolioSection.getBoundingClientRect();
     if(portfolioSectionPosition.y < 100 ) {
         showIcon();
+        setTimeout(() => {
+            animateLine();
+        }, 500) // this should sync the line with the icons
         window.removeEventListener('scroll', checkPortfolioPosition);
     }
 }
-
-window.addEventListener('scroll', checkPortfolioPosition);
-
-// Scroll down when clicking on landing page banner
-const scrollButton = document.getElementById('scroll-down-button');
-const downSection = document.getElementById('skills-container');
-const navHeight = document.querySelector('header nav').offsetHeight;
 
 function scrollDown() {
     let sectionRect = downSection.getBoundingClientRect();
@@ -54,4 +104,4 @@ function scrollDown() {
 }
 
 scrollButton.addEventListener('click', scrollDown);
-
+window.addEventListener('scroll', checkPortfolioPosition);
--- resources/index.js
+++ resources/index.js
@@ -17,7 +17,6 @@ const scrollPage = () => {
 
 }
 
-verticalList.addEventListener('click', scrollPage);
 
 // Slideshow for boardgame case page
 let currentIndex = 0;
@@ -64,3 +63,44 @@ function changeSlide(isNext) {
 
     showSlides();
 }
+
+// Enlarge images on click
+const caseImage = document.getElementsByClassName('case-picture');
+const pictureFrame = document.getElementsByClassName('picture-frame');
+
+function toogleZoomImage(element) {
+    if(element.className.indexOf('image-enlarged') !== -1) {
+        element.classList.remove('image-enlarged'); 
+    } else {
+        element.classList.add('image-enlarged');
+    }
+    
+}
+
+for(let i = 0; i < pictureFrame.length; i++) {
+    pictureFrame[i].addEventListener('click', function(e) {
+        const tagName = e.target.tagName.toLowerCase();
+        // if(tagName === 'div') {
+        //     toogleZoomImage(e.target)
+        // } else if(tagName === 'img'){
+        //     toogleZoomImage(e.target.parentElement)
+        // }            
+
+        toogleZoomImage(tagName === 'div' ? e.target : e.target.parentElement)
+    });
+}
+
+
+verticalList.addEventListener('click', scrollPage);
+const body = document.querySelector('body');
+
+body.addEventListener('click', function(e) {
+    if(e.target.className.indexOf('picture-frame') !== -1) {
+        return;
+    }
+    const images = document.querySelectorAll('.image-enlarged');
+
+    for(let img of images) {
+        img.classList.remove('image-enlarged');
+    }
+})
